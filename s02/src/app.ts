289d90e1a533700964815fc94956c5320ea9e6f6
TolunEsmanur <160126791+TolunEsmanur@users.noreply.github.com>
2024-10-22 21:05:21 +0300

    intersection

--- s02/src/app.ts
+++ s02/src/app.ts
@@ -32,13 +32,32 @@
 // let car4: Car = 2024
 
 //OBJECT
-const car: {
-    type: string,
-    model: string,
-    readonly year: number
-} = {
-    type: "Honda",
-    model:  "CRV",
-    year: 2024
-}
- car.year=2023 //compiler error
+// const car: {
+//     type: string,
+//     model: string,
+//     readonly year: number
+// } = {
+//     type: "Honda",
+//     model:  "CRV",
+//     year: 2024
+// }
+//  car.year=2023 //compiler error
+
+//**intersection**//
+// type Book = {
+//     book_id: number,
+//     book_name: string
+// }
+
+// type Author = {
+//     author_id: number,
+//     author_name: string
+// }
+
+// type Sales = Book & Author
+// let sale1: Sales ={
+//     author_id: 123,
+//     book_id: 456,
+//     author_name: "Tolkien",
+//     book_name: "Retırn of the King"
+// }
