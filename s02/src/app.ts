b3bb916852c75141707598655eabe092fb8d9e06
TolunEsmanur <160126791+TolunEsmanur@users.noreply.github.com>
2024-10-22 20:40:12 +0300

    type narrowing

--- s02/src/app.ts
+++ s02/src/app.ts
@@ -1,1 +1,18 @@
-console.log("S02");
+// console.log("S02");
+
+// let a : string | number = 0.3
+
+// a= "Hello"
+// a=false //compiler error
+
+// //**Type Narrowing**/
+
+// function checkNumber (n:(string | number)):void{
+//     n.
+//  if (typeof n==="number"){
+//     n.
+//  }else{
+//     n.
+//  }
+// }
+
